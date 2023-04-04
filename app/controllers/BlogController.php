be1ac503e3e8e20a618a2de45a5947be148bcae9
Eric Bostard <eric.bostard@gmail.com>
2023-04-03 21:55:36 -0400

    insert and update

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -8,15 +8,17 @@ class BlogController extends Controller{
 
     public function indexAction(){
         $db = DB::getInstance();
-        $sql = "INSERT INTO articles (`title`, `body`) VALUES (:title, :body)";
-        $bind = ['title' => 'new article', 'body' => 'article body'];
-        $query = $db->query($sql, $bind);
-        $lastID = $query->lastInsertId();
+        //$sql = "INSERT INTO articles (`title`, `body`) VALUES (:title, :body)";
+        //$bind = ['title' => 'new article', 'body' => 'article body'];
+        //$query = $db->query($sql, $bind);
+        //$lastID = $query->lastInsertId();
         //$db->execute($sql, $bind);
         //$sql ="SELECT * FROM articles";
         //$articles = $db->query($sql) -> results();
         //$count = $query->count();
-        H::dnd($lastID);
+        //H::dnd($lastID);
+        //$db->insert('articles', ['title' => 'article', 'body' => 'article body']);
+        $db->update('articles', ['title' => 'article updated', 'body' => 'body updated'], ['id' => '7']);
         $this->view->setSiteTitle('Newest Articles');
         $this->view->render();
     }
