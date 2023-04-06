b52f3e9c01cc1384f99c89b42222b63fda6faa03
Eric Bostard <eric.bostard@gmail.com>
2023-04-06 17:04:32 -0400

    Lots of validations

--- app/models/Users.php
+++ app/models/Users.php
@@ -2,7 +2,7 @@
 namespace App\Models;
 
 use Core\Model;
-use Core\Validators\{RequiredValidator, EmailValidator, MatchesValidator};
+use Core\Validators\{RequiredValidator, EmailValidator, MatchesValidator, MinValidator, MaxValidator, NumericValidator, UniqueValidator};
 
 class Users extends Model{
     protected static $table = 'users';
@@ -19,11 +19,15 @@ class Users extends Model{
         $this->runValidation(new RequiredValidator($this, ['field' => 'email', 'msg' => "email is a required field"]));
         $this->runValidation(new EmailValidator($this, ['field' =>'email', 'msg' => "must provide valid email"]));
         $this->runValidation(new RequiredValidator($this, ['field' => 'acl', 'msg' => "role is a required field"]));
-
+        // $this->runValidation(new NumericValidator($this, ['field' => 'fname', 'msg' => "Field must be numeric"]));
+        $this->runValidation(new UniqueValidator($this, ['field' => ['email', 'lname'], 'msg' => "A user with this email already exists"]));
+ 
         if($this->isNew()){
             $this->runValidation(new RequiredValidator($this, ['field' => 'password', 'msg' => "password is a required field"]));
             $this->runValidation(new RequiredValidator($this, ['field' => 'confirm', 'msg' => "Confirm password is a required field"]));
             $this->runValidation(new MatchesValidator($this, ['field' => 'confirm', 'rule' =>$this->password, 'msg' => "Passwords do not match"]));
+            $this->runValidation(new MinValidator($this, ['field' => 'password', 'rule' =>8, 'msg' => "Password must be at least 8 characters"]));
+            // $this->runValidation(new MaxValidator($this, ['field' => 'password', 'rule' =>10, 'msg' => "Password must be shorter than 10 characters"]));
 
             $this->password = password_hash($this->password, PASSWORD_DEFAULT);
         }
---
+++ core/validators/UniqueValidator.php
@@ -0,0 +1,31 @@
+<?php
+namespace Core\Validators;
+use Core\Validators\Validator;
+
+class UniqueValidator extends Validator{
+    public function runValidation(){
+        $value = $this->_obj->{$this->field};
+        if($value == '' || !isset($value)){
+            return true;
+        }
+       
+        $conditions = "{$this->field} = :{$this->field}";
+        $bind = [$this->field => $value];
+
+        //Check updating record
+        if(!$this->_obj->isNew()){
+            $conditions .= " AND id != :id";
+            $bind['id'] = $this->_obj->id;
+        }
+
+        //This allows to check multiple fields for unique
+        foreach($this->additionalFieldData as $adds){
+            $conditions .= " AND {$adds} = :{$adds}";
+            $bind[$adds] = $this->_obj->{$adds};
+        }
+
+        $queryParams = ['conditions' => $conditions, 'bind' => $bind];
+        $exists = $this->_obj::findFirst($queryParams);
+        return !$exists;
+    }
+}
